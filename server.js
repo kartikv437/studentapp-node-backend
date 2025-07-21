83ddae2f40c19088451e45a8da6aba139a3f2e51
kartikv437 <kartik.er.vit@gmail.com>
2025-07-21 18:52:48 +0530

    domain added

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
 const uploadRoutes = require('./routes/uploadRoutes');
 const applicationRoutes = require('./routes/applicationRoutes');
 const dotenv = require('dotenv');
 dotenv.config({ path: '.env' });
+const connectDB = require('./config/db');
 const app = express();
 app.use(cors({ origin: "*" }));
 app.use(express.json());
@@ -18,19 +18,7 @@ const stripe = require("stripe")("sk_test_51RjIueFVHBcv9MBMTsZIZisRZgcc3siuGnBXu
 // Stripe Account (er.kartik93@gmail.com) {https://dashboard.stripe.com/test/settings/user}
 // mongo DB Connection (kartikv437@gmail.com) {https://cloud.mongodb.com/v2/6811a4298f33665f0251b2e4#/metrics/replicaSet/6811a5b3ffd707258c2c9e2e/explorer/sample_mflix/comments/find}
 
-mongoose.Promise = global.Promise;
-
-const mongoUrl = process.env.REACT_APP_MONGODB;
-console.log(mongoUrl);
-mongoose.connect(mongoUrl, {
-    ssl: true,
-    tls: true
-}).then(() => {
-    console.log("Successfully connected to the database --- " + mongoUrl);
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
-    process.exit();
-});
+connectDB();
 
 app.use('/', uploadRoutes);
 
